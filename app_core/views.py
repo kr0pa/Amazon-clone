214901a332fdd88cce02b0b4ae871076e622b9a4
kr0pa <piotrkropagorski@gmail.com>
2024-09-03 19:22:07 +0200

    created basic structure main page and banner

--- app_core/views.py
+++ app_core/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+import requests
 
 def home(request):
-    return render(request, 'home.html')
+    response = requests.get("https://fakestoreapi.com/products?limit=20")
+    products = response.json()
+    
+    return render(request, 'home.html', {'products': products})
