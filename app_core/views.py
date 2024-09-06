e1343e3018b76a471e7420f8473601a2e3390cad
kr0pa <piotrkropagorski@gmail.com>
2024-09-06 22:22:30 +0200

    connect dummyjson api for list products

--- app_core/views.py
+++ app_core/views.py
@@ -2,14 +2,35 @@ from django.shortcuts import render
 import requests
 
 def home(request):
-    response = requests.get("https://fakestoreapi.com/products?limit=20")
-    products = response.json()
-    print(products[0])
+    # response = requests.get("https://fakestoreapi.com/products?limit=20")
+    # products = response.json()
+    # response = requests.get("https://api.escuelajs.co/api/v1/products")
+    # products = response.json()
+    response = requests.get("https://dummyjson.com/products")
+    data = response.json()
+
+    products = []
+    if 'products' in data:
+        for product in data['products']:
+            title = product['title']
+            description = product['description']
+            price = product['price']
+            image = product['images'][0]
+            
+            products.append({
+                'title': title,
+                'description': description,
+                'price': price,
+                'image': image
+            })
+    else:
+        print("Nie udało się pobrać danych produktów")
+            
     
     return render(request, 'home.html', {'products': products})
 
 def product(request, id):
-    response = requests.get("http://fakestoreapi.com/products/{}".format(id))
-    product = response.json()
+    # response = requests.get("http://fakestoreapi.com/products/{}".format(id))
+    # product = response.json()
     
     return render(request, 'product.html', {'product': product})
